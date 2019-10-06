d65b11e504fe0daa9b64270eeea8f30d1d2b07bc
Taylor Nelms <taylornelms15@gmail.com>
2019-10-06 17:00:33 -0400

    python script for data collection

---
+++ data/testAESrunner.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+import sys
+import os
+
+
+baseExecutablePath = "../build/Release/cis565_aes_test.exe"
+
+
+def main():
+    pass
+
+
+
+
+
+if __name__ == "__main__":
+    main()
